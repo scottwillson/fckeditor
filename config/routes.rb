48fe7a1c7cce3b077172409c50b5cedacae84ea9
Scott Willson <scott.willson@gmail.com>
2011-08-11 16:34:20 -0700

    Fix routing deprecation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails::Application.routes.draw do |map|
+Rails.application.routes.draw do
   match 'fckeditor/check_spelling' => 'fckeditor#check_spelling'
   match 'fckeditor/command' => 'fckeditor#command'
   match 'fckeditor/upload' => 'fckeditor#upload'
