905f4db427732e030da948726c49ef87b51e8f09
Scott Willson <scott.willson@gmail.com>
2011-08-11 17:06:45 -0700

    Drop custom routing code. MOved to config/routes.rb

--- init.rb
+++ init.rb
@@ -14,21 +14,3 @@ ActionView::Base.send(:include, Fckeditor::Helper)
 
 # require the controller
 require 'fckeditor_controller'
- add a route for spellcheck
-class ActionController::Routing::RouteSet
-  unless (instance_methods.include?('draw_with_fckeditor'))
-    class_eval <<-"end_eval", __FILE__, __LINE__  
-      alias draw_without_fckeditor draw
-      def draw_with_fckeditor
-        draw_without_fckeditor do |map|
-          map.connect 'fckeditor/check_spelling', :controller => 'fckeditor', :action => 'check_spelling'
-          map.connect 'fckeditor/command', :controller => 'fckeditor', :action => 'command'
-          map.connect 'fckeditor/upload', :controller => 'fckeditor', :action => 'upload'
-          yield map
-        end
-      end
-      alias draw draw_with_fckeditor
-    end_eval
-  end
-end
-
